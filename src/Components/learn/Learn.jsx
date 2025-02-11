cc30a5cde888d72df4ef22e03299043d325aad93
therealmensumit <sk03950@gmail.com>
2025-02-11 10:54:00 +0530

    animate text on 404 page

--- src/Components/learn/Learn.jsx
+++ src/Components/learn/Learn.jsx
@@ -1,25 +1,24 @@
 import { learn } from "../../staticData/learn";
 
 export const Learn = () => {
+  const { img, title, lists } = learn;
   return (
     <section className="py-20 bg-black text-white overflow-x-hidden">
       <div className="container">
-        <h2 className="text-[32px] md:text-[40px] !mb-10 line">
-          {learn.title}
-        </h2>
+        <h2 className="text-[32px] md:text-[40px] !mb-10 line">{title}</h2>
         <div className="grid gap-8 md:grid-cols-2">
           <div className="md:hidden">
             <img
-              src={learn.img}
-              alt={learn.title}
-              title={learn.title}
+              src={img}
+              alt={title}
+              title={title}
               className="w-full"
               loading="lazy"
             />
           </div>
           <div>
             <ul className="font-alegreya text-xl font-normal ps-8">
-              {learn?.lists?.map((list, i) => (
+              {lists?.map((list, i) => (
                 <li className="learn-list" key={i}>
                   {list}
                 </li>
@@ -28,9 +27,9 @@ export const Learn = () => {
           </div>
           <div className="learn-img-div">
             <img
-              src={learn.img}
-              alt={learn.title}
-              title={learn.title}
+              src={img}
+              alt={title}
+              title={title}
               className="w-full"
               loading="lazy"
             />
