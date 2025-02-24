1bbf39437d76bc3022781444ff1857adf5212316
therealmensumit <sk03950@gmail.com>
2025-02-24 20:25:06 +0530

    `feat: updated package.json, Hero component, and index.css files`

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -4,14 +4,16 @@ import { hero } from "../../staticData/hero";
 export const Hero = () => {
   return (
     <section className="hero-section">
-      <div className="hero">
-        <div className="line text-base">{hero.subTitle}</div>
-        <h1 className="text-balance mb-8 text-[32px] md:text-[40px] font-bold font-alegreya">
-          {hero.title}
-        </h1>
-        <Link to={hero.btnLink} role="button">
-          <button className="btn">{hero.btnText}</button>
-        </Link>
+      <div className="container relative w-full h-full">
+        <div className="hero">
+          <div className="line text-base">{hero.subTitle}</div>
+          <h1 className="text-balance mb-8 text-[32px] md:text-[40px] font-bold font-alegreya">
+            {hero.title}
+          </h1>
+          <Link to={hero.btnLink} role="button">
+            <button className="btn">{hero.btnText}</button>
+          </Link>
+        </div>
       </div>
     </section>
   );
